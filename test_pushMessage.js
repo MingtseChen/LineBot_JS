7bad18e1206e8a667276f533339be986155fc64b
ChenMT <gene31105@gmail.com>
2017-04-06 14:02:19 -0700

    modified:   test_pushMessage.js

--- test_pushMessage.js
+++ test_pushMessage.js
@@ -3,9 +3,9 @@ const line = require('./index')
 
 // init with auth token
 line.init({
-  accessToken: '{YOUR_ACCESS_TOKEN}',
+  accessToken: '4L2aRBb34xjalpFoMdeaTiSABsn4p6r5/cvVTbBnnOfB3Lzfu79gwW/Q3BU4HMVSiUbVPax7Eq++UEguxptioW72UCqgHO3PW9gaUVVZnAuSArf6RYP4gUYa8SIe3RRDniLOSbsRuafMJ5mu7lSojwdB04t89/1O/w1cDnyilFU=',
   // (Optional) for webhook signature validation
-  channelSecret: '{YOUR_CHANNEL_SECRET}'
+  channelSecret: 'fb5a231a8330f2438503cc5d4f9b2cc9'
 })
 
 line.client
