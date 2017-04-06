e039ba0f5520d1115d44ec4b065c4917246c3d1d
ChenMT <gene31105@gmail.com>
2017-04-06 14:27:42 -0700

    update 3

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-    channelId: channel Id,
-    channelSecret: channel Secret,
-    channelAccessToken: channel Access Token
+    channelId: '1508877129',
+    channelSecret: 'fb5a231a8330f2438503cc5d4f9b2cc9',
+    channelAccessToken: '4L2aRBb34xjalpFoMdeaTiSABsn4p6r5/cvVTbBnnOfB3Lzfu79gwW/Q3BU4HMVSiUbVPax7Eq++UEguxptioW72UCqgHO3PW9gaUVVZnAuSArf6RYP4gUYa8SIe3RRDniLOSbsRuafMJ5mu7lSojwdB04t89/1O/w1cDnyilFU='
 });
 
 bot.on('message', function(event) {
