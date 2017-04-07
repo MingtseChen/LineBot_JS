5736f53aae205b277ed0de63b006794d410a846d
ChenMT <gene31105@gmail.com>
2017-04-06 19:39:27 -0700

    config

--- setting.js
+++
@@ -1,6 +0,0 @@
-var bot = linebot({
-	channelId: '1508877129',
-	channelSecret: 'fb5a231a8330f2438503cc5d4f9b2cc9',
-	channelAccessToken: '4L2aRBb34xjalpFoMdeaTiSABsn4p6r5/cvVTbBnnOfB3Lzfu79gwW/Q3BU4HMVSiUbVPax7Eq++UEguxptioW72UCqgHO3PW9gaUVVZnAuSArf6RYP4gUYa8SIe3RRDniLOSbsRuafMJ5mu7lSojwdB04t89/1O/w1cDnyilFU=',
-	verify: true // Verify 'X-Line-Signature' header (default=true)
-});
