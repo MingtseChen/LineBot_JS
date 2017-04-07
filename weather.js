64e3eedc050881ac97cbf04de26e3a36aef9a534
ChenMT <gene31105@gmail.com>
2017-04-07 13:13:20 -0700

    building string verifier

--- weather.js
+++ weather.js
@@ -1,57 +1,73 @@
 var request = require('request');
 var jsonQuery = require('json-query');
 var stringCity = '台北';
+var DstringCity = '   台北市@中正區   ';
+var DstringCity = '   台北市@中正區   ';
 var stringCounty1 = '中正區';
 
-request({
-	uri: 'https://works.ioa.tw/weather/api/all.json',
-	method: 'GET',
-	timeut: '10000'
-}, function(error, response, body) {
-	if (response.statusCode == 200) {
-		console.log('Get Data Success', response && response.statusCode); // Print the response status code if a response was received
-		var data = JSON.parse(body);
-		//console.log(data[0].towns[1].name);
-		var query = '[*][*][name =' + stringCity + '][*towns][name = ' + stringCounty1 + ']';
-		var result = jsonQuery(query, {
-			data: data
-		}).value.id;
-		//console.log(data[0]);
-		//console.log('-----------------------------------');
-		//console.log(result);
-		request({
-			uri: 'https://works.ioa.tw/weather/api/weathers/' + result + '.json',
-			method: 'GET',
-			timeut: '10000'
-		}, function(error, response, body) {
-			console.log('Get Data Success', response && response.statusCode);
-			var data = JSON.parse(body);
-			var resultDesc = jsonQuery('', {
-				data: data
-			}).value.desc;
-			var resultTemp = jsonQuery('', {
-				data: data
-			}).value.temperature;
-			var resultRf = jsonQuery('', {
-				data: data
-			}).value.rainfall;
-			var resultAt = jsonQuery('', {
-				data: data
-			}).value.at;
-			console.log(stringCity + stringCounty1);
-			console.log(resultDesc);
-			console.log(resultTemp.slice(0,2)+"~"+resultTemp.slice(2) + " C");
-			console.log(resultRf + "%");
-			console.log(resultAt);
-		})
-	} else {
-		console.log('Connect Api Error:', error); // Print the error if one occurred
-	}
-	console.log('Success Get Replied End Weather Func');
-});
+trimInput(DstringCity);
+
+function trimInput(inStr) {
+	var getStringRAW = inStr.trim();
+	console.log(getStringRAW);
+	var city = getStringRAW.slice(0, getStringRAW.indexOf("@")-1);
+	console.log(city);
+	var dist = getStringRAW.slice(getStringRAW.indexOf("@") + 1);
+	console.log(dist);
+}
+
+
 
 function getCurrentWeather(location) {
 
+
+
+	request({
+		uri: 'https://works.ioa.tw/weather/api/all.json',
+		method: 'GET',
+		timeut: '10000'
+	}, function(error, response, body) {
+		if (response.statusCode == 200) {
+			console.log('Get Data Success', response && response.statusCode); // Print the response status code if a response was received
+			var data = JSON.parse(body);
+			//console.log(data[0].towns[1].name);
+			var query = '[*][*][name =' + stringCity + '][*towns][name = ' + stringCounty1 + ']';
+			var result = jsonQuery(query, {
+				data: data
+			}).value.id;
+			//console.log(data[0]);
+			//console.log('-----------------------------------');
+			//console.log(result);
+			request({
+				uri: 'https://works.ioa.tw/weather/api/weathers/' + result + '.json',
+				method: 'GET',
+				timeut: '10000'
+			}, function(error, response, body) {
+				console.log('Get Data Success', response && response.statusCode);
+				var data = JSON.parse(body);
+				var resultDesc = jsonQuery('', {
+					data: data
+				}).value.desc;
+				var resultTemp = jsonQuery('', {
+					data: data
+				}).value.temperature;
+				var resultRf = jsonQuery('', {
+					data: data
+				}).value.rainfall;
+				var resultAt = jsonQuery('', {
+					data: data
+				}).value.at;
+				console.log(stringCity + stringCounty1);
+				console.log(resultDesc);
+				console.log(resultTemp.slice(0, 2) + "~" + resultTemp.slice(2) + " C");
+				console.log(resultRf + "%");
+				console.log(resultAt);
+			})
+		} else {
+			console.log('Connect Api Error:', error); // Print the error if one occurred
+		}
+		console.log('Success Get Replied End Weather Func');
+	});
 }
 
 /*
