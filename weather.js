90c44615b89b601b9be32594140a5acf583057f1
ChenMT <gene31105@gmail.com>
2017-04-07 10:47:14 -0700

    add weather api

---
+++ weather.js
@@ -0,0 +1,29 @@
+var request = require('request');
+/*
+request({
+	uri:'https://works.ioa.tw/weather/api/all.json',
+	method:'GET',
+	timeut:'10000'
+}, function(error, response, body) {
+	console.log('error:', error); // Print the error if one occurred
+	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	//console.log('body:', body); // Print the HTML for the Google homepage.
+	var data = JSON.parse(body);
+	console.log(data[0].towns[1].name);
+});*/
+function getCurrentWeather(location){
+
+}
+
+
+request({
+	uri:'https://works.ioa.tw/weather/api/weathers/1.json',
+	method:'GET',
+	timeut:'10000'
+}, function(error, response, body) {
+	console.log('error:', error); // Print the error if one occurred
+	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	//console.log('body:', body); // Print the HTML for the Google homepage.
+	var data = JSON.parse(body);
+	console.log(data);
+});
