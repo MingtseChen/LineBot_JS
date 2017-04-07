a469318fa92c9eb21d746b46894229083eafb839
ChenMT <gene31105@gmail.com>
2017-04-06 19:28:33 -0700

    config

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var linebot = require('linebot');
 var express = require('express');
-var setting = require('setting');
+var setting = require('/setting.js');
 /*
 var bot = linebot({
-    channelId: '1508877129',
-    channelSecret: 'fb5a231a8330f2438503cc5d4f9b2cc9',
-    channelAccessToken: '4L2aRBb34xjalpFoMdeaTiSABsn4p6r5/cvVTbBnnOfB3Lzfu79gwW/Q3BU4HMVSiUbVPax7Eq++UEguxptioW72UCqgHO3PW9gaUVVZnAuSArf6RYP4gUYa8SIe3RRDniLOSbsRuafMJ5mu7lSojwdB04t89/1O/w1cDnyilFU='
+	channelId: '1508877129',
+	channelSecret: 'fb5a231a8330f2438503cc5d4f9b2cc9',
+	channelAccessToken: '4L2aRBb34xjalpFoMdeaTiSABsn4p6r5/cvVTbBnnOfB3Lzfu79gwW/Q3BU4HMVSiUbVPax7Eq++UEguxptioW72UCqgHO3PW9gaUVVZnAuSArf6RYP4gUYa8SIe3RRDniLOSbsRuafMJ5mu7lSojwdB04t89/1O/w1cDnyilFU='
 });
 /*
 bot.on('message', function(event) {
